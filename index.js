29748ace186cc764624a41c47c01a316edfbc0b5
vishalicious213 <vishalicious213@gmail.com>
2023-03-18 07:57:54 -0700

    removed #hero html from index.html and rendered it from index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// render wizard in hero div
+document.getElementById("hero").innerHTML = `
+    <div class="character-card">
+        <h4 class="name"> Wizard </h4>
+        <img class="avatar" src="images/wizard.png"/>
+        <p class="health">health: <b> 60 </b></p>
+        <div class="dice-container"><div class="dice"> 6 </div></div>
+    </div> 
+`
