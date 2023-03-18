558af22f40f155185f0e99cb6f8bdc6e711b66ba
vishalicious213 <vishalicious213@gmail.com>
2023-03-18 08:26:34 -0700

    index.js: removed hero and monster html blocks and drafted renderCharacter()

--- index.js
+++ index.js
@@ -12,22 +12,14 @@ const monsterAvatar = "images/orc.png"
 const monsterHealth = "10"
 const monsterDiceRoll = 4
 
-// render wizard in hero div
-document.getElementById("hero").innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Wizard </h4>
-        <img class="avatar" src="images/wizard.png"/>
-        <p class="health">health: <b> 60 </b></p>
-        <div class="dice-container"><div class="dice"> 6 </div></div>
-    </div> 
-`
-
-// render orc in monster div
-document.getElementById("monster").innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Orc </h4>
-        <img class="avatar" src="images/orc.png"/>
-        <p class="health">health: <b> 10 </b></p>
-        <div class="dice-container"><div class="dice"> 4 </div></div>
-    </div>
-`
+// render hero or monster characters
+function renderCharacter(elementID, name, avatar, health, diceRoll) {
+    document.getElementById(elementID).innerHTML = `
+        <div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}"/>
+            <p class="health">health: <b> ${health} </b></p>
+            <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+        </div>
+    `
+}
