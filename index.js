e5ac440f63d5bf64a046782687dcc1478ba4d40f
vishalicious213 <vishalicious213@gmail.com>
2023-03-18 07:59:10 -0700

    removed #monster html from index.html and rendered it from index.js

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ document.getElementById("hero").innerHTML = `
         <div class="dice-container"><div class="dice"> 6 </div></div>
     </div> 
 `
+
+// render orc in monster div
+document.getElementById("monster").innerHTML = `
+    <div class="character-card">
+        <h4 class="name"> Orc </h4>
+        <img class="avatar" src="images/orc.png"/>
+        <p class="health">health: <b> 10 </b></p>
+        <div class="dice-container"><div class="dice"> 4 </div></div>
+    </div>
+`
