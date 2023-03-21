808a4d6596151a9306aa3e4859cf79befdd7613e
vishalicious213 <vishalicious213@gmail.com>
2023-03-20 23:08:43 -0700

    drafted Character.js and moved Character() constructor function into it. made it an export default and imported it into index.js. imported getDiceRollArray() into Character.js and removed import from index.js

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
 import characterData from "./data.js"
-import { getDiceRollArray } from "./utils.js"
-
-// character constructor function
-function Character(data) {
-    Object.assign(this, data)
-
-    this.getCharacterHtml = function() {
-        const { elementId, name, avatar, health, diceCount } = this
-        const diceHtml = this.getDiceHtml(diceCount)
-        // document.getElementById(elementId).innerHTML = `
-        return `
-            <div class="character-card">
-                <h4 class="name"> ${name} </h4>
-                <img class="avatar" src="${avatar}"/>
-                <p class="health">health: <b> ${health} </b></p>
-                <div class="dice-container"> ${diceHtml} </div>
-            </div>
-        `
-    }
-
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(die) {
-            return `<div class="dice">${die}</div>`
-        }).join("")
-    }
-}
+import Character from "./Character.js"
 
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
