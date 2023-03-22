1db640c617dd14a93f7d2fe10cc40e5db6ec8096
vishalicious213 <vishalicious213@gmail.com>
2023-03-22 12:21:38 -0700

    index.js: attack(): added check if wizard or orc are dead, to run endGame()

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function attack() {
     wizard.takeDamage(orc.currentDiceScore)
     orc.takeDamage(wizard.currentDiceScore)
     render()
+
+    if (wizard.dead || orc.dead) {
+        endGame()
+    }
 }
 
 function endGame() {
