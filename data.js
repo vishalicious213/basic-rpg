e720bb624447404c7ed011e40c3808ddb0d94b7a
vishalicious213 <vishalicious213@gmail.com>
2023-03-23 23:42:40 -0700

    data.js: renamed monster object to orc. added demon & goblin objects

--- data.js
+++ data.js
@@ -2,15 +2,29 @@ const characterData = {
     hero: {
         name: "Wizard",
         avatar: "images/wizard.png",
-        health: "60",
+        health: 60,
         diceCount: 3,
         currentDiceScore: []
     },
-    monster: {
+    orc: {
         name: "Orc",
         avatar: "images/orc.png",
-        health: "10",
+        health: 10,
         diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin.png",
+        health: 20,
+        diceCount: 3,
         currentDiceScore: []
     }
 }
