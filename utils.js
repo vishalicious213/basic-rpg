ff6535f3ac82d21722c9c2787ffef77f4d9341dc
vishalicious213 <vishalicious213@gmail.com>
2023-03-24 14:08:05 -0700

    utils.js: converted getPlaceholderHtml() logic into arrow function

--- utils.js
+++ utils.js
@@ -7,9 +7,9 @@ function getDiceRollArray(diceCount) {
 
 // start game with blank dice until they're rolled
 function getDicePlaceholderHtml(diceCount) {
-    return new Array(diceCount).fill(0).map(function() {
-        return `<div class="placeholder-dice"></div>`
-    }).join("")
+    return new Array(diceCount).fill(0).map(() => 
+        `<div class="placeholder-dice"></div>`
+    ).join("")
 }
 
 const getPercentage = (remainingHealth, maximumHealth) => 
